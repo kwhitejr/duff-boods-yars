2240d781a76cb2b33924a9c534ad682f10b400cc
Emmanuel Pilande <epilande@gmail.com>
2016-03-09 14:10:41 -1000

    Configure store

---
+++ src/store.js
@@ -0,0 +1,27 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { hashHistory } from 'react-router';
+import { syncHistory } from 'react-router-redux';
+import rootReducer from 'reducers';
+
+const router = syncHistory(hashHistory);
+
+const middleware = [thunk, router];
+
+const enhancers = compose(
+  applyMiddleware(thunk, router)
+);
+
+export default function configureStore(initialState) {
+  const store = createStore(rootReducer, initialState, enhancers);
+
+  router.listenForReplays(store);
+
+  if (module.hot) {
+    module.hot.accept('./reducers', () =>
+      store.replaceReducer(require('./reducers'))
+    );
+  }
+
+  return store;
+}
