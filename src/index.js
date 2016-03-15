1b3ebd2ede00a1faf958e64b4cbfda17a677e467
Emmanuel Pilande <epilande@gmail.com>
2016-03-14 14:02:46 -1000

    Rename `app.global.css` -> `app.css`

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { syncHistoryWithStore } from 'react-router-redux';
 
 import routes from 'routes';
 import configureStore from 'store';
-import 'app.global.css';
+import 'app.css';
 
 const store = configureStore();
 const history = syncHistoryWithStore(hashHistory, store);
