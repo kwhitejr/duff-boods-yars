db508994865a03099fc57ac6c1b27056cdddba1a
Emmanuel Pilande <epilande@gmail.com>
2016-03-16 18:32:22 -1000

    Fix ExtractTextPlugin in production build
    ExtractTextPlugin wasn't set up correctly

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -17,8 +17,7 @@ config.module.loaders.push({
   test: /\.css$/,
   loader: ExtractTextPlugin.extract(
     'style-loader',
-    'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
-    'postcss-loader'
+    'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'
   )
 });
 
