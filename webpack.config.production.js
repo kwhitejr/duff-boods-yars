6d89c454fadbddab9ee5150763e14350675e2fe3
Emmanuel Pilande <epilande@gmail.com>
2016-03-17 23:57:59 -1000

    Add cssnext & remove autoprefixer & rucksack
    Use cssnext autoprefixer

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -26,8 +26,7 @@ config.postcss = function postcss() {
     require('postcss-modules-values'),
     require('postcss-import'),
     require('postcss-nested'),
-    require('rucksack-css'),
-    require('autoprefixer')({ browsers: ['last 2 versions'] }),
+    require('postcss-cssnext')({ browsers: ['last 2 versions', 'IE > 10'] }),
   ];
 };
 
