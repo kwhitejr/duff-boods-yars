049047eb11d07e528a971c127784bc2313fbb59b
Emmanuel Pilande <epilande@gmail.com>
2016-04-12 01:13:55 -1000

    Add onFocus & autoFocus attribute to Input component

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -3,21 +3,25 @@ import React, { PropTypes } from 'react';
 const Input = ({
   id,
   className,
+  autoFocus,
   type,
   placeholder,
   value,
   onBlur,
   onChange,
+  onFocus,
   onKeyDown,
 }) => (
   <input
     id={id}
+    autoFocus={autoFocus}
     className={className}
     type={type}
     placeholder={placeholder}
     value={value}
     onBlur={onBlur}
     onChange={onChange}
+    onFocus={onFocus}
     onKeyDown={onKeyDown}
   />
 );
@@ -29,11 +33,13 @@ Input.defaultProps = {
 Input.propTypes = {
   id: PropTypes.string,
   className: PropTypes.string,
+  autoFocus: PropTypes.bool,
   type: PropTypes.string,
   placeholder: PropTypes.string,
   value: PropTypes.string,
   onBlur: PropTypes.func,
   onChange: PropTypes.func,
+  onFocus: PropTypes.func,
   onKeyDown: PropTypes.func,
 };
 
