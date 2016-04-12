dcdd2353ff3a7f07d0b328dbe451bc3b0ed44bc7
Emmanuel Pilande <epilande@gmail.com>
2016-04-11 20:46:08 -1000

    Change ListItem to be a stateful component

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,25 +1,61 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+import Input from 'components/Input';
 import styles from './ListItem.css';
 
-const ListItem = ({ item, onRemove }) => {
-  let removeButton;
-  if (onRemove) {
-    removeButton = <button className={styles.remove} onClick={() => onRemove(item)}>X</button>;
+class ListItem extends Component {
+  static propTypes = {
+    item: PropTypes.object.isRequired,
+    onRemove: PropTypes.func,
+    onEdit: PropTypes.func,
+  };
+
+  constructor(props) {
+    super();
+    this._onChange = this._onChange.bind(this);
+    this._onRemove = this._onRemove.bind(this);
+    this.state = {
+      text: props.item.item || '',
+    };
   }
 
-  return (
-    <li className={styles.item}>
-      {item.item} {removeButton}
-    </li>
-  );
-};
+  _onChange(event) {
+    this.setState({ text: event.target.value });
+  }
 
-ListItem.defaultProps = {
-};
+  _onRemove() {
+    const { item, onRemove } = this.props;
+    onRemove(item);
+  }
 
-ListItem.propTypes = {
-  item: PropTypes.object.isRequired,
-  onRemove: PropTypes.func,
-};
+
+
+  render() {
+    const { item, onRemove, onEdit } = this.props;
+
+    let removeButton;
+    if (onRemove) {
+      removeButton = <button className={styles.remove} onClick={this._onRemove}>X</button>;
+    }
+
+    let text;
+    if (onEdit) {
+      text = (
+        <Input
+          className={styles.editable}
+          onChange={this._onChange}
+          value={this.state.text}
+        />
+      );
+    } else {
+      text = this.state.text;
+    }
+
+    return (
+      <li className={styles.item}>
+        {text} {removeButton}
+      </li>
+    );
+  }
+}
 
 export default ListItem;
