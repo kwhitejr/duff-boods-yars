02bcd24aa9d7f84c126279e1abcab0c01344366c
Emmanuel Pilande <epilande@gmail.com>
2016-04-11 18:02:22 -1000

    Display `X` icon on list item
    If onRemove function is passed to List component, display `X` icon on list item

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,8 +1,18 @@
 import React, { PropTypes } from 'react';
 import styles from './List.css';
 
-const List = ({ items }) => {
-  const ListItems = items.map((item) => <li className={styles.item} key={item.id}>{item.item}</li>);
+const List = ({ items, onRemove }) => {
+  const ListItems = items.map((item) => {
+    let removeButton;
+    if (onRemove) {
+      removeButton = <button onClick={() => onRemove(item)}>X</button>;
+    }
+    return (
+      <li className={styles.item} key={item.id}>
+        {item.item} {removeButton}
+      </li>
+    );
+  });
 
   return (
     <ul className={styles.list}>
@@ -16,6 +26,7 @@ List.defaultProps = {
 
 List.propTypes = {
   items: PropTypes.arrayOf(PropTypes.object),
+  onRemove: PropTypes.func,
 };
 
 export default List;
