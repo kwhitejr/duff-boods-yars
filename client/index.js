2bb9f7a5630570cabef12a22ed8915d38f06c3ea
kwhitejr <kwhitejr.edu@gmail.com>
2016-12-01 21:14:09 -1000

    load token to auth on initialize

--- client/index.js
+++ client/index.js
@@ -3,14 +3,23 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, hashHistory } from 'react-router/es6';
 import { syncHistoryWithStore } from 'react-router-redux';
+import cookie from 'react-cookie';
 import { install as offlineInstall } from 'offline-plugin/runtime'; // eslint-disable-line
 
 import routes from 'routes';
 import configureStore from 'store';
+import { AUTH_USER } from 'actions/types';
 import 'styles/app.css';
 
 const store = configureStore();
 const history = syncHistoryWithStore(hashHistory, store);
+
+const token = cookie.load('token');
+
+if (token) {
+  // Update application state. User has token and is probably authenticated
+  store.dispatch({ type: AUTH_USER });
+}
 
 render(
   <Provider store={store}>
