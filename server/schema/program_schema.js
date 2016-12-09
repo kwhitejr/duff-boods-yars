a6e8c9b1c9c588b65b6213db895ca6f4ac472597
kwhitejr <kwhitejr.edu@gmail.com>
2016-12-08 21:20:59 -1000

    save program with relational user_id

--- server/schema/program_schema.js
+++ server/schema/program_schema.js
@@ -1,17 +1,18 @@
 import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
 
 //= ===============================
 // Program Schema
 //= ===============================
 const programSchema = new Schema({
-  user_id: { type: String, required: true }, // FK User
-  last_modified: { type: Date, required: true },
-  start_date: { type: Date, required: false },
-  stop_date: { type: Date, required: false },
-  program_type: { type: String, required: true },
-  workouts: { type: Array, default: [] },
+  user_id:        { type: ObjectId, required: true }, // FK User
+  last_modified:  { type: Date, required: true },
+  start_date:     { type: Date, required: false },
+  stop_date:      { type: Date, required: false },
+  program_type:   { type: String, required: true },
+  workouts:       { type: Array, default: [] },
 });
 
 export default mongoose.model('Program', programSchema);
